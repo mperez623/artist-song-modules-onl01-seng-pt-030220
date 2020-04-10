fdfbdeb42876d19c43fe42e96c3ccaad907016f1
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-10 19:36:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Paramble::InstanceMethods
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
